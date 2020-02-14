05292c235e0984a5a39b317c93854fd43662760a
anorman149 <anormanmail@gmail.com>
2020-02-14 17:01:10 -0500

    Correct items back to SPA

--- web/src/app/component/home/home.component.ts
+++ web/src/app/component/home/home.component.ts
@@ -8,7 +8,13 @@ import {MessageService} from 'primeng/api';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  private collapsed = true; // Start collapsed Navigation
+  private collapsed: boolean = true; //Start collapsed Navigation
+  private showBudgets: boolean = false;
+  private showDashboard: boolean = true; //Default View
+  private showTransactions: boolean = false;
+  private showAccount: boolean = false;
+  private showSettings: boolean = false;
+  private showAPI: boolean = false;
 
   constructor(private authenticationService: AuthenticationService,
               private messageService: MessageService) {
@@ -19,7 +25,16 @@ export class HomeComponent implements OnInit {
 
   }
 
-  logout() {
+  hideAll(){
+    this.showBudgets = false;
+    this.showDashboard = false;
+    this.showTransactions = false;
+    this.showAccount = false;
+    this.showSettings = false;
+    this.showAPI = false;
+  }
+
+  logout(){
     this.authenticationService.logout();
   }
 }
