57daa73aa190e30a25104052942251a314d11ed8
anorman149 <anormanmail@gmail.com>
2020-02-20 20:00:11 -0500

    Removed Plaid items

--- server/src/main/java/com/ninebudget/model/InstitutionVendor.java
+++
@@ -1,9 +0,0 @@
-package com.ninebudget.model;
-
-import java.util.List;
-
-@Vendor
-public interface InstitutionVendor {
-    List<InstitutionAccount> getAccounts() throws InstitutionException;
-
-}
