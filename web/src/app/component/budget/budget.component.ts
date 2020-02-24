d0fc7104240862da2fb66046db102caee59972f2
Rosanna Corvino-Rosa <rosanna.corvino@SHE-MB1446.local>
2020-02-24 15:00:38 -0500

    Add new structure for budget page

--- web/src/app/component/budget/budget.component.ts
+++ web/src/app/component/budget/budget.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {Budget} from '../../model/budget';
-import {BudgetService} from '../../service/budget.service';
-import {PrimaryComponent} from '../../model/primary-component';
-import {BudgetTiming} from '../../model/budget-timing.model';
+import {IBudget} from "../../model/budget";
+import {BudgetService} from "../../service/budget.service";
+import {PrimaryComponent} from "../../model/primary-component";
+import {CategoryType} from "../../model/category-type.model";
 
 @Component({
   selector: 'app-budget',
@@ -10,21 +10,62 @@ import {BudgetTiming} from '../../model/budget-timing.model';
   styleUrls: ['./budget.component.scss']
 })
 export class BudgetComponent implements OnInit, PrimaryComponent {
-  budget: Budget;
+  budgets?: IBudget[];
+  leftOverColor = "";
+  leftOverAmount = 0;
+  spent = 0;
+  income = 0;
+  budgetNewDisplay: boolean = false;
 
   constructor(private budgetService: BudgetService) {}
 
   ngOnInit() {
-    const budget: Budget = new Budget();
-    budget.name = 'a';
-    budget.amount = 12;
-    budget.budgetTiming = BudgetTiming.BIWEEKLY;
-    budget.useLeftOver = false;
-    budget.active = true;
+    this.budgetService.getAll().subscribe(
+      data => {
+        this.budgets = data ? data : [];
 
-    this.budgetService.get(budget).subscribe(data => {
-      this.budget = data;
-    });
+        //Loop through Each Budget and set appropriate Values
+        for(let budget of this.budgets){
+          budget.amountSpent = 0;
+
+          //Calculate Necessary items with Transactions
+          for(let trans of budget.transactions){
+            budget.amountSpent += trans.amount; //Set Amount Spent for Budget
+
+            //Gather Spent vs Income
+            if(trans.category.type == CategoryType.INCOME){
+              this.income += trans.amount; //Add to total Income
+            }else{
+              this.spent += trans.amount; //Add to total spent
+            }
+          }
+
+          //Calculate Money left Over
+          this.leftOverAmount = this.income - this.spent;
+
+          //Assign CSS Value for leftOver
+          this.leftOverColor = 'value-up'; //Start Positive
+          if(this.leftOverAmount < 0){
+            this.leftOverColor = 'value-down';
+          }
+        }
+      },
+      error => console.log("ERROR") //TODO
+    );
+  }
+
+  checkProgressType(budget: IBudget){
+    //Set Progress Bar Type
+    let progressType = "";  //Set for Goal Met
+    if(budget.amountSpent > budget.amount){
+      //Set for Failure
+      progressType = "danger";
+    }else if(budget.amountSpent < budget.amount){
+      //Set for Goal Not Yet Met
+      progressType = "success";
+    }
+
+    return progressType;
   }
 
   create(object: any) {
