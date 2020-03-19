e7fb24f7006841e4fc8599154bc6c1d6f4c504b3
anorman149 <anormanmail@gmail.com>
2020-03-18 20:18:15 -0400

    Correcting account services...

--- web/src/app/service/account.service.ts
+++ web/src/app/service/account.service.ts
@@ -1,36 +1,46 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Account} from '../model/account';
+import {HttpClient, HttpHeaders, HttpParams, HttpResponse} from '@angular/common/http';
+import {Account, IAccount} from '../model/account';
 import {PrimaryService} from '../model/primary-service';
 import {Observable} from 'rxjs';
+import {api} from "../../environments/environment";
+import {createRequestOption} from "../shared/util/request-util";
+
+type EntityResponseType = HttpResponse<IAccount>;
+type EntityArrayResponseType = HttpResponse<IAccount[]>;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService implements PrimaryService {
-  private readonly accountUrl: string;
-
-  constructor(private http: HttpClient) {
-    this.accountUrl = '/9budget/api/v1/accounts';
-  }
+  constructor(private http: HttpClient) {}
 
   public getAll() {
-    const headers = new HttpHeaders().set('Content-Type', 'application/json');
-
-    return this.http.get<Account[]>(this.accountUrl, {headers});
+    return this.http.get<Account[]>(api.url + api.account.url );
   }
 
   public get(account: Account) {
-    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http.get<Account>(api.url + api.account.url + '/' + account.id);
+  }
 
-    return this.http.get<Account>(this.accountUrl + '/' + account.id, {headers});
+  public query(req?: any): Observable<EntityArrayResponseType> {
+    const options = createRequestOption(req);
+    return this.http.get<IAccount[]>(api.url + api.account.url, { params: options, observe: 'response' });
   }
 
   public create(account: Account) {
-    return this.http.post<Account>(this.accountUrl + '/' + account.id, account);
+    return this.http.put<Account>(api.url + api.account.url, account);
   }
 
   public update(account: Account) {
-    return this.http.put<Account>(this.accountUrl + '/' + account.id, account);
+    return this.http.post<Account>(api.url + api.account.url, account);
+  }
+
+  public delete(account: Account) {
+    let httpParams = new HttpParams().set('id', String(account.id));
+
+    let options = { params: httpParams };
+
+    return this.http.delete<Account>(api.url + api.account.url, options);
   }
 }
